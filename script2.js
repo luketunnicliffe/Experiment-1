01dbaa92e05aaef7e74906284163b68784eecca3
luketunnicliffe <luketunnicliffe@gmail.com>
2021-10-11 14:05:04 +0900

    Changed higher-order function info

--- script2.js
+++ script2.js
@@ -21,3 +21,25 @@ const hello = () => {
   is2p2();
   
   console.log(is2p2.name);
+
+
+  const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+    for(let i = 1; i <= 1000000; i++) {
+      if ( (2 + 2) != 4) {
+        console.log('Something has gone very wrong :( ');
+      }
+    }
+  };
+  
+  const addTwo = num => num + 2;
+  
+  const timeFuncRuntime = funcParameter => {
+    let t1 = Date.now();
+    funcParameter();
+    let t2 = Date.now();
+    return t2 - t1;
+  };
+  
+  // Write your code below
+  
+  
